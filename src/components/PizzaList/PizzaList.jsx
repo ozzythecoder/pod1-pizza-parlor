26f637b9a08e8c764e8fce8c2c55a1e72c8b54f3
August McAllister <august.chm@gmail.com>
2022-12-01 11:08:19 -0600

    Moved pizza list to own component

---
+++ src/components/PizzaList/PizzaList.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function PizzaList() {
+
+  const fetchPizzas = () => {
+
+    axios.get('/api/pizza')
+      .then(res => {
+        console.log('got pizzas');
+
+      }).catch(err => console.log('fetchPizzas', err))
+
+  }
+
+  useEffect(() => {
+    fetchPizzas();
+  }, [])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
--- src/components/PizzaMenu/PizzaMenu.jsx
+++ src/components/PizzaMenu/PizzaMenu.jsx
@@ -1,28 +1,11 @@
-import axios from "axios";
-import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 export default function PizzaMenu() {
 
   const history = useHistory();
-
-  const fetchPizzas = () => {
-
-    axios.get('/api/pizza')
-      .then(res => {
-        console.log('got pizzas');
-
-      }).catch(err => console.log('fetchPizzas', err))
-
-  }
-
-  const continueOrder = () => {
+ const continueOrder = () => {
     history.push('/customerinfo');
   }
-
-  useEffect(() => {
-    fetchPizzas();
-  }, [])
 
   return (<div>
     <button onClick={continueOrder}>Next</button>
