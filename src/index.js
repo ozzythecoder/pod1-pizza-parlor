81f0df46d2c3a5cf2961d07f3e2cb2a7bcfc1a5b
August McAllister <august.chm@gmail.com>
2022-12-01 10:45:14 -0600

    Redux imported

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+import App from './components/App/App';
+import './index.css';
+
+const currentOrder = (state = [], action) => {
+
+  
+  return state;
+}
+
+const reduxStore = createStore(
+  combineReducers(
+    {}
+  ),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={reduxStore}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
