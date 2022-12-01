1e8713faaffa7cb10eb9aeaff0dcea8517a829aa
Andrew Carey <andrew.r.carey@gmail.com>
2022-12-01 10:45:49 -0600

    created router, and wrapped a home page in the first Route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route} from 'react-router-dom';
+
 
 function App() {
 
@@ -9,9 +11,12 @@ function App() {
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
+      <Router>
+        <Route exact path = '/'>
+          <img src='images/pizza_photo.png' />
+          <p>Pizza is great.</p>
+        </Route>
+      </Router>
   
     </div>
   );
