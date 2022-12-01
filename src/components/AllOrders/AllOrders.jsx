9657a10f8cbb2c4939a05c2107a7da600c9e91ee
Andrew Carey <andrew.r.carey@gmail.com>
2022-12-01 13:11:53 -0600

    created tbody to nest table rows

--- src/components/AllOrders/AllOrders.jsx
+++ src/components/AllOrders/AllOrders.jsx
@@ -1,20 +1,45 @@
 import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 
 function AllOrders() {
-    axios.get('/api/order')
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(err => console.log(err));
+    const [orders, setOrders] = useState([]);
 
+    const fetchOrders = () => {
+        axios.get('/api/order')
+            .then(response => {
+                console.log(response.data);
+                setOrders(response.data);
+            })
+            .catch(err => console.log(err));
+    }
 
+    useEffect(() => {
+        fetchOrders()
+    }, [] );
 
     return (
         <table>
-            <tr>
-                <th>##TODO insert table here</th>
-            </tr>
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <th>Time Order Placed</th>
+                    <th>Type</th>
+                    <th>Cost</th>
+                </tr>
+                
+                {orders.map(order => {
+                    return (
+                    <tr key={order.id}>
+                        <td>{order.customer_name}</td>
+                        <td>{order.time}</td>
+                        <td>{order.type}</td>
+                        <td>{order.total}</td>
+                    </tr>
+                    )
+                })}
+            </tbody>
             
             
         </table>
