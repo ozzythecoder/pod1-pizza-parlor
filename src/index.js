689f3e836e033639afeddd4b9a5282b5e0ea0bf5
August McAllister <august.chm@gmail.com>
2022-12-01 10:46:13 -0600

    Added order placeholder

--- src/index.js
+++ src/index.js
@@ -8,9 +8,25 @@ import logger from 'redux-logger';
 import App from './components/App/App';
 import './index.css';
 
-const currentOrder = (state = [], action) => {
+const orderPlaceholder = {
+  "customer_name": "Donatello",
+  "street_address": "20 W 34th St",
+  "city": "New York",
+  "zip": "10001",
+  "total": "27.98",
+  "type": "Pickup",
+  "pizzas": [{
+    "id": "1",
+    "quantity": "1"
+  },{
+    "id": "2",
+    "quantity": "1"
+  }]
+}
 
-  
+const currentOrder = (state = orderPlaceholder, action) => {
+
+
   return state;
 }
 
